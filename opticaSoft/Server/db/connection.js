aee38cca6b352546b1c013507b7e2206bf703e2e
michasion0908 <michasion0908@gmail.com>
2026-05-14 16:09:13 -0600

    login y variables de entorno configurados

--- opticaSoft/Server/db/connection.js
+++ opticaSoft/Server/db/connection.js
@@ -2,7 +2,9 @@ import mysql from 'mysql2/promise';
 
 export const db = await mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '12345',
-  database: process.env.DB_NAME || 'opticadb'
+  database: process.env.DB_NAME || 'opticadb',
+  ssl: process.env.DB_HOST?.includes('aivencloud.com') ? { rejectUnauthorized: false } : false
 });
