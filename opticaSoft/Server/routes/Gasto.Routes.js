8c89c5902c060d70dd1076f859847d06247e3db6
michasion0908 <michasion0908@gmail.com>
2026-06-05 15:59:46 -0600

    fix id_Gasto en handleUpdate y handleDelete

--- opticaSoft/Server/routes/Gasto.Routes.js
+++ opticaSoft/Server/routes/Gasto.Routes.js
@@ -14,7 +14,7 @@ router.get("/", getGastos);
 router.get("/mes/:mes/:anio", getGastosByMes);
 router.get("/resumen/:mes/:anio", getResumenMensual);
 router.post("/create", createGasto);
-router.put("/update/:idGasto", updateGasto);
-router.delete("/delete/:idGasto", deleteGasto);
+router.put("/update/:id_Gasto", updateGasto);
+router.delete("/delete/:id_Gasto", deleteGasto);
 
 export default router;
